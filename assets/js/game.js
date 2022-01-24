54065b3ce05e953132577ec6dcdbdf424f6cdb08
troat9824 <troat9824@gmail.com>
2022-01-24 04:30:16 -0600

    startgame function

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,10 +74,30 @@ var fight = function(enemyName) {
     }
   };
 
+  var endGame = function() {
+    if (playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".")
+    }
+    else {
+      window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+  
+    if (playAgainConfirm) {
+      startGame();
+    }
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+  };
 
-
-// fight each enemy-robot by looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++) {
+// function to start a new game
+var startGame = function() {
+  // fight each enemy-robot by looping over them and fighting them one at a time
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
     // if player is still alive, keep fighting
     if (playerHealth > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -101,3 +121,9 @@ for (var i = 0; i < enemyNames.length; i++) {
       break;
     }
   }
+  // play again
+  endGame();
+}
+
+startGame();
+
