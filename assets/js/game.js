8ca389c290041433dadce503f46121b5e0f45d25
troat9824 <troat9824@gmail.com>
2022-02-09 01:21:14 -0600

    playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,19 @@
 //     * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    window.prompt("What is your robot's name?");
+  };
+
+  console.log("Your robot's name is " + name);
+  return name;
+
+};
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -169,7 +180,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
+
       // pick new enemy to fight based on the index of the enemy.names array
       var pickedEnemyObj = enemyInfo[i];
   
